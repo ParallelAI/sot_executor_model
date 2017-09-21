f5892112e1ef9059ac3298e9009641be0b6763f7
tamasjambor <jamborta@gmail.com>
2017-09-21 14:15:52 +0100

    updated dag and step types

--- src/test/scala/parallelai/sot/executor/model/JSONDefinitionsSpec.scala
+++ src/test/scala/parallelai/sot/executor/model/JSONDefinitionsSpec.scala
@@ -29,13 +29,21 @@ class JSONDefinitionsSpec extends WordSpec with Matchers {
 
       val inSchema = PubSubSchemaType(`type` = "PubSub", serialization = "Avro", name = "Message", schema = avroSchema, "p2pin")
       val outSchema = DatastoreSchemaType(`type` = "Datastore", schema = Some(DatastoreSchema(name = "OutSchema", fields = List(DatastoreSchemaField(name = "score2x", `type` = "Double")))))
-      val dag = List(DAGMapping(from = "filter", to = "mapper1"),
+      val dag = List(
+        DAGMapping(from = "source1", to = "filter"),
+        DAGMapping(from = "filter", to = "mapper1"),
         DAGMapping(from = "mapper1", to = "sumByKey"),
-        DAGMapping(from = "sumByKey", to = "mapper2"))
-      val steps = List(Op(`type` = "transformation", name = "mapper1", op = "map", func = "m => (m.teamName, m.score.toInt)"),
+        DAGMapping(from = "sumByKey", to = "mapper2"),
+        DAGMapping(from = "mapper2", to = "sink1")
+      )
+      val steps = List(
+        Op(`type` = "source", name = "source1", op = "", func = ""),
+        Op(`type` = "transformation", name = "mapper1", op = "map", func = "m => (m.teamName, m.score.toInt)"),
         Op(`type` = "transformation", name = "filter", op = "filter", func = "m => m.score > 2"),
         Op(`type` = "transformation", name = "mapper2", op = "map", func = "m => OutSchema(m._1, m._2.toString, m._2 * 0.123)"),
-        Op(`type` = "transformation", name = "sumByKey", op = "sumByKey", func = ""))
+        Op(`type` = "transformation", name = "sumByKey", op = "sumByKey", func = ""),
+        Op(`type` = "sink", name =  "sink1", op = "", func = "")
+      )
 
       val expectedConfig = Config(inSchema, outSchema, dag, steps)
       expectedConfig should be(config)
