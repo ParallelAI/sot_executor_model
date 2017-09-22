704dd0980aed76b6ce3f4a3cdf02f670c72299de
tamasjambor <jamborta@gmail.com>
2017-09-22 14:14:25 +0100

    refactored schema definition

--- src/main/scala/parallelai/sot/executor/model/JSONDefinitions.scala
+++ src/main/scala/parallelai/sot/executor/model/JSONDefinitions.scala
@@ -12,27 +12,36 @@ object SOTMacroConfig {
 
   sealed trait SchemaType
 
-  case class PubSubSchemaType(`type`: String, serialization: String, name: String, schema: JsObject, topic: String) extends SchemaType
+  case class PubSubSchemaType(`type`: String, name: String, serialization: String, definition: JsObject, topic: String) extends SchemaType
 
-  case class BigQuerySchemaType(`type`: String, name: String, schema: JsObject, dataset: String, table: String) extends SchemaType
+  case class BigQuerySchemaType(`type`: String, name: String, definition: JsObject, dataset: String, table: String) extends SchemaType
 
-  case class BigTableSchemaType(`type`: String, instanceId: String, tableId: String, familyName: List[String], numNodes: Int) extends SchemaType
+  case class BigTableSchemaType(`type`: String, name: String, instanceId: String, tableId: String, familyName: List[String], numNodes: Int) extends SchemaType
 
-  case class DatastoreSchemaField(name: String, `type`: String)
-  case class DatastoreSchema(name: String, fields: List[DatastoreSchemaField])
-  case class DatastoreSchemaType(`type`: String, schema: Option[DatastoreSchema]) extends SchemaType
+  case class DatastoreSchemaField(`type`: String, name: String)
+
+  case class DatastoreSchemaDefinition(name: String, fields: List[DatastoreSchemaField])
+
+  case class DatastoreSchemaType(`type`: String, name: String, kind: String, definition: Option[DatastoreSchemaDefinition]) extends SchemaType
 
   case class DAGMapping(from: String, to: String) extends Topology.Edge[String]
-  case class Config(in: SchemaType, out: SchemaType, dag: List[DAGMapping], steps: List[Op]) {
 
-    def parseDAG() : Topology[String, DAGMapping] = {
+  case class Config(schemas: List[SchemaType], dag: List[DAGMapping], steps: List[OpType]) {
+
+    def parseDAG(): Topology[String, DAGMapping] = {
       val vertices = (dag.map(_.from) ++ dag.map(_.to)).distinct
       Topology.createTopology(vertices, dag)
     }
 
   }
 
-  case class Op(`type`: String, name: String, op: String, func: String)
+  sealed trait OpType
+
+  case class TransformationOp(`type`: String, name: String, op: String, func: String) extends OpType
+
+  case class SourceOp(`type`: String, name: String, schema: String) extends OpType
+
+  case class SinkOp(`type`: String, name: String, schema: String) extends OpType
 
 }
 
@@ -42,11 +51,11 @@ object SOTMacroJsonConfig {
 
   implicit val avroSchemaFormat = jsonFormat5(PubSubSchemaType)
   implicit val bigQueryFormat = jsonFormat5(BigQuerySchemaType)
-  implicit val bigTableFormat = jsonFormat5(BigTableSchemaType)
+  implicit val bigTableFormat = jsonFormat6(BigTableSchemaType)
 
   implicit val datastoreSchemaFieldFormat = jsonFormat2(DatastoreSchemaField)
-  implicit val datastoreSchemaFormat = jsonFormat2(DatastoreSchema)
-  implicit val datastoreFormat = jsonFormat2(DatastoreSchemaType)
+  implicit val datastoreSchemaDefinitionFormat = jsonFormat2(DatastoreSchemaDefinition)
+  implicit val datastoreSchemaFormat = jsonFormat4(DatastoreSchemaType)
 
   implicit object SchemaJsonFormat extends RootJsonFormat[SchemaType] {
 
@@ -60,34 +69,34 @@ object SOTMacroJsonConfig {
 
     def read(value: JsValue): SchemaType = {
       value.asJsObject.getFields("type") match {
-        case Seq(JsString(typ)) if typ == "PubSub" => {
-          value.asJsObject.getFields("type", "serialization", "name", "schema", "topic") match {
-            case Seq(JsString(objType), JsString(serialization), JsString(name), schema, JsString(topic)) =>
-              PubSubSchemaType(`type` = objType, serialization = serialization, name = name, schema = schema.asJsObject, topic = topic)
+        case Seq(JsString(typ)) if typ == "pubsub" => {
+          value.asJsObject.getFields("type", "serialization", "name", "definition", "topic") match {
+            case Seq(JsString(objType), JsString(serialization), JsString(name), definition, JsString(topic)) =>
+              PubSubSchemaType(`type` = objType, serialization = serialization, name = name, definition = definition.asJsObject, topic = topic)
             case _ => deserializationError("PubSub expected")
           }
         }
-        case Seq(JsString(typ)) if typ == "BigQuery" => {
-          value.asJsObject.getFields("type", "name", "schema", "dataset", "table") match {
-            case Seq(JsString(objType), JsString(name), schema, JsString(dataset), JsString(table)) =>
-              BigQuerySchemaType(`type` = objType, name = name, schema = schema.asJsObject, table = table, dataset = dataset)
+        case Seq(JsString(typ)) if typ == "bigquery" => {
+          value.asJsObject.getFields("type", "name", "definition", "dataset", "table") match {
+            case Seq(JsString(objType), JsString(name), definition, JsString(dataset), JsString(table)) =>
+              BigQuerySchemaType(`type` = objType, name = name, definition = definition.asJsObject, table = table, dataset = dataset)
             case _ => deserializationError("BigQuery expected")
           }
         }
-        case Seq(JsString(typ)) if typ == "BigTable" => {
-          value.asJsObject.getFields("type", "instanceId", "tableId", "familyName", "numNodes") match {
-            case Seq(JsString(objType), JsString(instanceId), JsString(tableId), familyName, JsNumber(numNodes)) =>
+        case Seq(JsString(typ)) if typ == "bigtable" => {
+          value.asJsObject.getFields("type", "name", "instanceId", "tableId", "familyName", "numNodes") match {
+            case Seq(JsString(objType), JsString(name), JsString(instanceId), JsString(tableId), familyName, JsNumber(numNodes)) =>
               val fn = familyName.convertTo[List[String]]
-              BigTableSchemaType(`type` = objType, instanceId = instanceId, tableId = tableId, familyName = fn, numNodes = numNodes.toInt)
+              BigTableSchemaType(`type` = objType, name = name, instanceId = instanceId, tableId = tableId, familyName = fn, numNodes = numNodes.toInt)
             case _ => deserializationError("BigTable expected")
           }
         }
-        case Seq(JsString(typ)) if typ == "Datastore" => {
-          value.asJsObject.getFields("type", "schema") match {
-            case Seq(JsString(objType), JsObject(schema)) =>
-              DatastoreSchemaType(`type` = objType, schema = Some(schema.toJson.convertTo[DatastoreSchema]))
-            case Seq(JsString(objType)) =>
-              DatastoreSchemaType(`type` = objType, schema = None)
+        case Seq(JsString(typ)) if typ == "datastore" => {
+          value.asJsObject.getFields("type", "name", "definition", "kind") match {
+            case Seq(JsString(objType), JsString(name), JsObject(definition), JsString(kind)) =>
+              DatastoreSchemaType(`type` = objType, name = name, kind = kind, definition = Some(definition.toJson.convertTo[DatastoreSchemaDefinition]))
+            case Seq(JsString(objType), JsString(name), JsString(kind)) =>
+              DatastoreSchemaType(`type` = objType, name = name, kind = kind, definition = None)
             case _ => deserializationError("Datastore expected")
           }
         }
@@ -96,9 +105,51 @@ object SOTMacroJsonConfig {
     }
   }
 
-  implicit val opFormat = jsonFormat4(Op)
+  implicit val transformationOpFormat = jsonFormat4(TransformationOp)
+  implicit val sinkOpFormat = jsonFormat3(SinkOp)
+  implicit val sourceOpFormat = jsonFormat3(SourceOp)
+
+  implicit object OpJsonFormat extends RootJsonFormat[OpType] {
+
+    def write(c: OpType): JsValue = {
+      c match {
+        case s: TransformationOp => s.toJson
+        case s: SinkOp => s.toJson
+        case s: SourceOp => s.toJson
+      }
+    }
+
+    def read(value: JsValue): OpType = {
+      value.asJsObject.getFields("type") match {
+        case Seq(JsString(typ)) if typ == "source" => {
+          value.asJsObject.getFields("type", "name", "schema") match {
+            case Seq(JsString(objType), JsString(name), JsString(schema)) =>
+              SourceOp(`type` = objType, name = name, schema = schema)
+            case _ => deserializationError("SourceOp type expected")
+          }
+        }
+        case Seq(JsString(typ)) if typ == "sink" => {
+          value.asJsObject.getFields("type", "name", "schema") match {
+            case Seq(JsString(objType), JsString(name), JsString(schema)) =>
+              SinkOp(`type` = objType, name = name, schema = schema)
+            case _ => deserializationError("SinkOp type expected")
+          }
+        }
+        case Seq(JsString(typ)) if typ == "transformation" => {
+          value.asJsObject.getFields("type", "name", "op", "func") match {
+            case Seq(JsString(objType), JsString(name), JsString(op), JsString(func)) =>
+              TransformationOp(`type` = objType, name = name, op = op, func = func)
+            case _ => deserializationError("TransformationOp type expected")
+          }
+        }
+        case _ => deserializationError("SchemaType expected")
+      }
+    }
+  }
+
+
   implicit val dagFormat = jsonFormat2(DAGMapping)
-  implicit val configFormat = jsonFormat4(Config)
+  implicit val configFormat = jsonFormat3(Config)
 
   def apply(fileName: String): Config = {
     val stream: InputStream = getClass.getResourceAsStream("/" + fileName)
--- src/test/scala/parallelai/sot/executor/model/JSONDefinitionsSpec.scala
+++ src/test/scala/parallelai/sot/executor/model/JSONDefinitionsSpec.scala
@@ -2,21 +2,202 @@ package parallelai.sot.executor.model
 
 import org.scalatest.{Matchers, WordSpec}
 import parallelai.sot.executor.model.SOTMacroConfig._
+import parallelai.sot.executor.model.SOTMacroJsonConfig._
 import spray.json._
 
 class JSONDefinitionsSpec extends WordSpec with Matchers {
 
   "SOTMacroConfig" should {
 
-    "parse pubsub to datastore json string" in {
-      val config = SOTMacroJsonConfig("ps2ds-test.json")
-
-      val avroSchema =
+    "build pubsub to bigquery config" in {
+      val config = SOTMacroJsonConfig("ps2bq-test.json")
+      val schema =
         """
-          |{      "type": "record",
+          |{
+          |      "type": "record",
+          |      "name": "MessageExtended",
+          |      "namespace": "parallelai.sot.avro",
+          |      "fields": [
+          |        {
+          |          "name": "user",
+          |          "type": "string",
+          |          "doc": "Name of the user"
+          |        },
+          |        {
+          |          "name": "teamName",
+          |          "type": "string",
+          |          "doc": "Name of the team"
+          |        },
+          |        {
+          |          "name": "score",
+          |          "type": "int",
+          |          "doc": "User score"
+          |        },
+          |        {
+          |          "name": "eventTime",
+          |          "type": "long",
+          |          "doc": "time when event created"
+          |        },
+          |        {
+          |          "name": "eventTimeStr",
+          |          "type": "string",
+          |          "doc": "event time string for debugging"
+          |        },
+          |        {
+          |          "name": "count",
+          |          "type": "int",
+          |          "doc": "example count"
+          |        }
+          |      ],
+          |      "doc": "A basic schema for storing user records"
+          |    }
+        """.stripMargin.parseJson.asJsObject
+
+      val schemaOut =
+        """
+          |{
+          |      "fields": [
+          |        {
+          |          "mode": "REQUIRED",
+          |          "name": "user",
+          |          "type": "STRING"
+          |        },
+          |        {
+          |          "mode": "REQUIRED",
+          |          "name": "total_score",
+          |          "type": "INTEGER"
+          |        },
+          |        {
+          |          "mode": "REQUIRED",
+          |          "name": "processing_time",
+          |          "type": "STRING"
+          |        }
+          |      ]
+          |    }
+        """.stripMargin.stripMargin.parseJson.asJsObject
+
+      val schemas = List(PubSubSchemaType(`type` = "pubsub", name = "pubsub1", serialization = "Avro", definition = schema, topic = "p2pout"),
+        BigQuerySchemaType(`type` = "bigquery", name = "bigquery1", definition = schemaOut, dataset = "bigquerytest", table = "streaming_word_extract26"))
+
+      val dag = List(
+        DAGMapping(from = "in", to = "filter"),
+        DAGMapping(from = "filter", to = "mapper1"),
+        DAGMapping(from = "mapper1", to = "sumByKey"),
+        DAGMapping(from = "sumByKey", to = "mapper2"),
+        DAGMapping(from = "mapper2", to = "out")
+      )
+      val steps = List(
+        SourceOp(`type` = "source", name = "in", schema = "pubsub1"),
+        TransformationOp(`type` = "transformation", name = "mapper1", op = "map", func = "m => (m.teamName, m.score.toInt)"),
+        TransformationOp(`type` = "transformation", name = "filter", op = "filter", func = "m => m.score > 2"),
+        TransformationOp(`type` = "transformation", name = "mapper2", op = "map", func = "m => BigQueryRow(m._1, m._2, Helper.fmt.print(Instant.now()))"),
+        TransformationOp(`type` = "transformation", name = "sumByKey", op = "sumByKey", func = ""),
+        SinkOp(`type` = "sink", name = "out", schema = "bigquery1")
+      )
+
+      val expectedConfig = Config(schemas = schemas, dag = dag, steps = steps)
+      expectedConfig should be(config)
+
+    }
+
+    "build pubsub to bigtable config" in {
+      val config = SOTMacroJsonConfig("ps2bt-test.json")
+
+      val schema1 =
+        """
+          |{
+          |      "type": "record",
           |      "name": "Message",
           |      "namespace": "parallelai.sot.avro",
           |      "fields": [
+          |        {
+          |          "name": "user",
+          |          "type": "string",
+          |          "doc": "Name of the user"
+          |        },
+          |        {
+          |          "name": "teamName",
+          |          "type": "string",
+          |          "doc": "Name of the team"
+          |        },
+          |        {
+          |          "name": "score",
+          |          "type": "int",
+          |          "doc": "User score"
+          |        },
+          |        {
+          |          "name": "eventTime",
+          |          "type": "long",
+          |          "doc": "time when event created"
+          |        },
+          |        {
+          |          "name": "eventTimeStr",
+          |          "type": "string",
+          |          "doc": "event time string for debugging"
+          |        }
+          |      ],
+          |      "doc": "A basic schema for storing user records"
+          |}
+        """.stripMargin.parseJson.asJsObject
+
+      val inSchema = PubSubSchemaType(`type` = "pubsub", name = "pubsub1", serialization = "Avro", definition =  schema1, topic = "p2pin")
+      val outSchema = BigTableSchemaType(`type` = "bigtable", name = "bigtable1" ,instanceId = "bigtable-test",
+        tableId = "bigquerytest", familyName = List("cf"), numNodes = 3)
+
+      val schemas = List(inSchema, outSchema)
+
+      val dag = List(
+        DAGMapping(from = "in", to = "filter"),
+        DAGMapping(from = "filter", to = "mapper1"),
+        DAGMapping(from = "mapper1", to = "sumByKey"),
+        DAGMapping(from = "sumByKey", to = "mapper2"),
+        DAGMapping(from = "mapper2", to = "out")
+      )
+
+      val steps = List(
+        SourceOp(`type` = "source", name = "in", schema = "pubsub1"),
+        TransformationOp(`type` = "transformation", name = "mapper1", op = "map", func = "m => (m.teamName, m.score.toInt)"),
+        TransformationOp(`type` = "transformation", name = "filter", op = "filter", func = "m => m.score > 2"),
+        TransformationOp(`type` = "transformation", name = "mapper2", op = "map", func = "m => BigTableRecord(m._1, (\"cf\", \"counter\", m._2), (\"cf\", \"counter2\", m._2 * 1.225))"),
+        TransformationOp(`type` = "transformation", name = "sumByKey", op = "sumByKey", func = ""),
+        SinkOp(`type` = "sink", name = "out", schema = "bigtable1")
+      )
+
+      val expectedConfig = Config(schemas = schemas, dag = dag, steps = steps)
+      expectedConfig should be(config)
+
+    }
+
+    "build pubsub to pubsub config" in {
+      val config = SOTMacroJsonConfig("ps2ps-test.json")
+
+      val schema1 =
+        """
+          |{
+          |      "type": "record",
+          |      "name": "Message",
+          |      "namespace": "parallelai.sot.avro",
+          |      "fields": [
+          |        {
+          |          "name": "user",
+          |          "type": "string",
+          |          "doc": "Name of the user"
+          |        },
+          |        {
+          |          "name": "teamName",
+          |          "type": "string",
+          |          "doc": "Name of the team"
+          |        },
+          |        {
+          |          "name": "score",
+          |          "type": "int",
+          |          "doc": "User score"
+          |        },
+          |        {
+          |          "name": "eventTime",
+          |          "type": "long",
+          |          "doc": "time when event created"
+          |        },
           |        {
           |          "name": "eventTimeStr",
           |          "type": "string",
@@ -27,25 +208,220 @@ class JSONDefinitionsSpec extends WordSpec with Matchers {
           |    }
         """.stripMargin.parseJson.asJsObject
 
-      val inSchema = PubSubSchemaType(`type` = "PubSub", serialization = "Avro", name = "Message", schema = avroSchema, "p2pin")
-      val outSchema = DatastoreSchemaType(`type` = "Datastore", schema = Some(DatastoreSchema(name = "OutSchema", fields = List(DatastoreSchemaField(name = "score2x", `type` = "Double")))))
+      val schema2 =
+        """
+          |{
+          |      "type": "record",
+          |      "name": "MessageExtended",
+          |      "namespace": "parallelai.sot.avro",
+          |      "fields": [
+          |        {
+          |          "name": "user",
+          |          "type": "string",
+          |          "doc": "Name of the user"
+          |        },
+          |        {
+          |          "name": "teamName",
+          |          "type": "string",
+          |          "doc": "Name of the team"
+          |        },
+          |        {
+          |          "name": "score",
+          |          "type": "int",
+          |          "doc": "User score"
+          |        },
+          |        {
+          |          "name": "eventTime",
+          |          "type": "long",
+          |          "doc": "time when event created"
+          |        },
+          |        {
+          |          "name": "eventTimeStr",
+          |          "type": "string",
+          |          "doc": "event time string for debugging"
+          |        },
+          |        {
+          |          "name": "count",
+          |          "type": "int",
+          |          "doc": "example count"
+          |        }
+          |      ],
+          |      "doc": "A basic schema for storing user records"
+          |    }
+        """.stripMargin.stripMargin.parseJson.asJsObject
+
+      val inSchema = PubSubSchemaType(`type` = "pubsub", name = "pubsub1", serialization = "Avro", definition = schema1, topic = "p2pin")
+      val outSchema = PubSubSchemaType(`type` = "pubsub", name = "pubsub2", serialization = "Avro", definition = schema2, topic = "p2pout")
+
+      val schemas = List(inSchema, outSchema)
+
       val dag = List(
-        DAGMapping(from = "source1", to = "filter"),
+        DAGMapping(from = "in", to = "mapper1"),
+        DAGMapping(from = "mapper1", to = "out")
+      )
+
+      val steps = List(
+        SourceOp(`type` = "source", name = "in", schema = "pubsub1"),
+        TransformationOp(`type` = "transformation", name = "mapper1", op = "map", func = "m => MessageExtended(m.user, m.teamName, m.score, m.eventTime, m.eventTimeStr, 1)"),
+        SinkOp(`type` = "sink", name = "out", schema = "pubsub2")
+      )
+
+      val expectedConfig = Config(schemas = schemas, dag = dag, steps = steps)
+      expectedConfig should be(config)
+
+    }
+
+    "build pubsub to datastore config" in {
+      val config = SOTMacroJsonConfig("ps2ds-test.json")
+
+      val schema1 =
+        """
+          |{
+          |      "type": "record",
+          |      "name": "Message",
+          |      "namespace": "parallelai.sot.avro",
+          |      "fields": [
+          |        {
+          |          "name": "user",
+          |          "type": "string",
+          |          "doc": "Name of the user"
+          |        },
+          |        {
+          |          "name": "teamName",
+          |          "type": "string",
+          |          "doc": "Name of the team"
+          |        },
+          |        {
+          |          "name": "score",
+          |          "type": "int",
+          |          "doc": "User score"
+          |        },
+          |        {
+          |          "name": "eventTime",
+          |          "type": "long",
+          |          "doc": "time when event created"
+          |        },
+          |        {
+          |          "name": "eventTimeStr",
+          |          "type": "string",
+          |          "doc": "event time string for debugging"
+          |        }
+          |      ],
+          |      "doc": "A basic schema for storing user records"
+          |    }
+        """.stripMargin.stripMargin.parseJson.asJsObject
+
+      val inSchema = PubSubSchemaType(`type` = "pubsub", serialization = "Avro", name = "pubsub1", definition = schema1, topic = "p2pin")
+      val outSchema = DatastoreSchemaType(`type` = "datastore", name = "datastore1", kind = "kind1", definition =  None)
+
+      val schemas = List(inSchema, outSchema)
+
+      val dag = List(
+        DAGMapping(from = "in", to = "filter"),
         DAGMapping(from = "filter", to = "mapper1"),
         DAGMapping(from = "mapper1", to = "sumByKey"),
         DAGMapping(from = "sumByKey", to = "mapper2"),
-        DAGMapping(from = "mapper2", to = "sink1")
-      )
-      val steps = List(
-        Op(`type` = "source", name = "source1", op = "", func = ""),
-        Op(`type` = "transformation", name = "mapper1", op = "map", func = "m => (m.teamName, m.score.toInt)"),
-        Op(`type` = "transformation", name = "filter", op = "filter", func = "m => m.score > 2"),
-        Op(`type` = "transformation", name = "mapper2", op = "map", func = "m => OutSchema(m._1, m._2.toString, m._2 * 0.123)"),
-        Op(`type` = "transformation", name = "sumByKey", op = "sumByKey", func = ""),
-        Op(`type` = "sink", name =  "sink1", op = "", func = "")
+        DAGMapping(from = "mapper2", to = "out")
       )
 
-      val expectedConfig = Config(inSchema, outSchema, dag, steps)
+      val steps = List(
+        SourceOp(`type` = "source", name = "in", schema = "pubsub1"),
+        TransformationOp(`type` = "transformation", name = "mapper1", op = "map", func = "m => (m.teamName, m.score.toInt)"),
+        TransformationOp(`type` = "transformation", name = "filter", op = "filter", func = "m => m.score > 2"),
+        TransformationOp(`type` = "transformation", name = "mapper2", op = "map", func = "m => 'teamscores ->> m._1 :: 'score1 ->> m._2.toString :: 'score2 ->> (m._2 * 0.123) :: HNil"),
+        TransformationOp(`type` = "transformation", name = "sumByKey", op = "sumByKey", func = ""),
+        SinkOp(`type` = "sink", name = "out", schema = "datastore1")
+      )
+
+      val expectedConfig = Config(schemas = schemas, dag = dag, steps = steps)
+      expectedConfig should be(config)
+
+    }
+
+    "build pubsub to datastore with schema config" in {
+
+      val config = SOTMacroJsonConfig("ps2ds-with-schema-test.json")
+
+      val schema1 =
+        """
+          |{
+          |      "type": "record",
+          |      "name": "Message",
+          |      "namespace": "parallelai.sot.avro",
+          |      "fields": [
+          |        {
+          |          "name": "user",
+          |          "type": "string",
+          |          "doc": "Name of the user"
+          |        },
+          |        {
+          |          "name": "teamName",
+          |          "type": "string",
+          |          "doc": "Name of the team"
+          |        },
+          |        {
+          |          "name": "score",
+          |          "type": "int",
+          |          "doc": "User score"
+          |        },
+          |        {
+          |          "name": "eventTime",
+          |          "type": "long",
+          |          "doc": "time when event created"
+          |        },
+          |        {
+          |          "name": "eventTimeStr",
+          |          "type": "string",
+          |          "doc": "event time string for debugging"
+          |        }
+          |      ],
+          |      "doc": "A basic schema for storing user records"
+          |    }
+        """.stripMargin.stripMargin.parseJson.asJsObject
+
+      val schema2 =
+        """{
+          |      "name": "OutSchema",
+          |      "fields": [
+          |        {
+          |          "name": "teamscores",
+          |          "type": "String"
+          |        },
+          |        {
+          |          "name": "score1",
+          |          "type": "String"
+          |        },
+          |        {
+          |          "name": "score2",
+          |          "type": "Double"
+          |        }
+          |      ]
+          |    }
+        """.stripMargin.stripMargin.parseJson.convertTo[DatastoreSchemaDefinition]
+
+      val inSchema = PubSubSchemaType(`type` = "pubsub", serialization = "Avro", name = "pubsub1", definition = schema1, topic = "p2pin")
+      val outSchema = DatastoreSchemaType(`type` = "datastore", name = "datastore1", kind = "kind1", definition = Some(schema2))
+
+      val schemas = List(inSchema, outSchema)
+
+      val dag = List(
+        DAGMapping(from = "in", to = "filter"),
+        DAGMapping(from = "filter", "mapper1"),
+        DAGMapping(from = "mapper1", to = "sumByKey"),
+        DAGMapping(from = "sumByKey", to = "mapper2"),
+        DAGMapping(from = "mapper2", to = "out")
+      )
+
+      val steps = List(
+        SourceOp(`type` = "source", name = "in", schema = "pubsub1"),
+        TransformationOp(`type` = "transformation", name = "mapper1", op = "map", func = "m => (m.teamName, m.score.toInt)"),
+        TransformationOp(`type` = "transformation", name = "filter", op = "filter", func = "m => m.score > 2"),
+        TransformationOp(`type` = "transformation", name = "mapper2", op = "map", func = "m => OutSchema(m._1, m._2.toString, m._2 * 0.123)"),
+        TransformationOp(`type` = "transformation", name = "sumByKey", op = "sumByKey", func = ""),
+        SinkOp(`type` = "sink", name = "out", schema = "datastore1")
+      )
+
+      val expectedConfig = Config(schemas = schemas, dag = dag, steps = steps)
       expectedConfig should be(config)
 
     }
