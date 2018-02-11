0bb86360dcaa4bcb0e3d340155db85c07a0fb7b1
David Ainslie <dainslie@gmail.com>
2018-02-11 01:33:05 +0000

    Replaced implicit objects with implicits vals

--- build.sbt
+++ build.sbt
@@ -24,6 +24,7 @@ lazy val root = (project in file("."))
       scalaTest % Test
     ),
     libraryDependencies ++= Seq(
+      shapeless,
       sprayJson
     )
   )
--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -3,5 +3,7 @@ import sbt._
 object Dependencies {
   val scalaTest = "org.scalatest" %% "scalatest" % "3.0.3"
 
+  val shapeless = "com.chuusai" %% "shapeless" % "2.3.3"
+
   val sprayJson = "io.spray" %% "spray-json" % "1.3.3"
 }
